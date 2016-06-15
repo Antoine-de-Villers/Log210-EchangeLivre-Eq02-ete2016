bf243c427f64faef1514243bfb545439c98290b1
Vincent <vincent.deguille@live.fr>
2016-06-14 23:02:36 -0400

    VenteController créé

---
+++ Côté C#/MvcApplication1/Controllers/VenteController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class VenteController : Controller
+    {
+        //
+        // GET: /Vente
+
+        public ActionResult EffectuerVente()
+        {
+            return View();
+        }
+
+        //
+        //POST: /Vente
+
+
+
+    }
+}
